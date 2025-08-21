7147d30d919638e9256a29c409963e12dc4fa016
Mathias Frydensberg Sørensen <au617928@uni.au.dk>
2025-08-21 11:43:46 +0200

    github continuous checking

--- tests/testthat/test-my_filter.R
+++ tests/testthat/test-my_filter.R
@@ -1,4 +1,6 @@
-test_that("multiplication works", {
+test_that("my_filter() should work with row indices", {
   expect_equal(dim(my_filter(iris, 1:3)), c(3, ncol(iris)))
+  expect_equal(dim(my_filter(iris, -1)), c(nrow(iris) - 1, ncol(iris)))
+  expect_equal(dim(my_filter(iris, 1)), c(1, ncol(iris)))
   expect_s3_class(my_filter(iris, 1), "data.frame")
 })
